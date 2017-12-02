d9bf44fe308f10f02c89e4c60630ca503510e8e0
Jared <Jared@Littles-MacBook-Pro.local>
2017-12-02 10:58:00 -0500

    hello

--- assets/nyt.js
+++ assets/nyt.js
@@ -1,1 +1,23 @@
-7518943c027a43f28a592c595519229c
+//7518943c027a43f28a592c595519229c
+
+	
+var topicSearch = $(this).attr("data-name");
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "7518943c027a43f28a592c595519229c", 'q': "taco bell"
+});
+//console.log(response);
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result.docs.snippet);
+}).fail(function(err) {
+  throw err;
+
+});
+
+
+
+
